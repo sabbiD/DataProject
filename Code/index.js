b8aca9c9dbf8e6240d337ba669be65ae3836927a
sabbiD <36192972+sabbiD@users.noreply.github.com>
2018-06-09 23:22:44 +0200

    trying interactive map

--- Code/index.js
+++ Code/index.js
@@ -1,4 +1,4 @@
-//window.onload = function() {
+window.onload = function() {
 
 winterDeath = "Data/winterDeath.json";
 soilUse = "Data/soilUse.json";
@@ -6,6 +6,7 @@ tempData = "Data/tempData.json";
 specificPest = "Data/specificPest.json";
 winterDeathUS = "Data/winterDeathUS.json";
 mapDutch = "Data/provinces.geojson";
+pest = "Data/pest.json"
 
 d3.queue()
   .defer(d3.request, winterDeath)
@@ -14,6 +15,7 @@ d3.queue()
   .defer(d3.request, specificPest)
   .defer(d3.request, winterDeathUS)
   .defer(d3.request, mapDutch)
+  .defer(d3.request, pest)
   .awaitAll(callback);
 
 
@@ -26,8 +28,43 @@ function callback(error, response) {
 	specificPest = JSON.parse(response[3].responseText)
 	winterDeathUS = JSON.parse(response[4].responseText)
 	mapDutch = JSON.parse(response[5].responseText)
+	pest = JSON.parse(response[6].responseText)
 
-	createMap(mapDutch);
-	createRose(specificPest);
+	years = winterDeathNL["Winter bee death in the Netherlands"]["0"]
+	deathLines = Object.values(years)
+	years = Object.keys(years)
+	temps = Object.values(tempData["Temperatuur Midden-Nederland"])["0"]
+	temps = Object.values(temps)
+	//pesticides = Object.values(specificPest["Gewasbeschermingsmiddelen gebruik"]["0"])
+	pestTotal = Object.values(pest)
+	
+	//console.log(pest)
+	//console.log(deathLines)
+	//console.log(temps)
+	//console.log(specificPest)
+
+	var linesData = []
+
+	for (var i = 0; i < years.length - 4;i++){
+			
+			var n = i + 21
+			linesData.push({
+				
+				key: years[i],
+				values:{"death" : deathLines[i], "temp": temps[i + 11], 
+				"pest": pestTotal[i + 21]["Totaal"] }
+			})
+
 	}
-//}
+
+	//console.log(linesData)
+	createMap(mapDutch);
+	//createRose(specificPest);
+
+	var chart = createLines(years, 'values', [deathLines,
+        temps], {xAxis: 'Years', yAxis: 'Percentage'});
+    chart.bind("#lineGraph");
+    chart.render();
+
+	}
+}
