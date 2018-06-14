530e34ccda724190295550f17b951806a199d15a
sabbiD <36192972+sabbiD@users.noreply.github.com>
2018-06-14 13:28:49 +0200

    progress rose

--- Code/rose.js
+++ Code/rose.js
@@ -1,3 +1,4 @@
+// https://jsfiddle.net/forwardsprint/wrgtj1nL/ (rose chart)
 
 function createRose(dataset){
 	
@@ -14,7 +15,7 @@ function createRose(dataset){
 	  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 	var arc;
 
-	var categories = [data[0].Categorie, data[1].Categorie, data[2].Categorie, data[3].Categorie]
+	var categories = d3.scaleOrdinal().range(Object.keys(dataset[0][1995]))
 
 	var pie = d3.pie()
 	  .sort(null)
@@ -37,7 +38,7 @@ function createRose(dataset){
 	  arc = d3.arc()
 	    .outerRadius(radius)
 	    .innerRadius(radius - 4);
-	  radius = radius - 5;
+	  radius = radius - 4;
 
 	  g.append("path")
 	    .attr("d", arc)
@@ -48,15 +49,17 @@ function createRose(dataset){
 	    .style("stroke-width", 3);
 
 	}
-
+	
 	var startAngle = 0;
-	for (var i = 0; i < 4; i++) {
+	for (var i = 1; i < 5; i++) {
+	  
 	  arc = d3.arc()
-	    .innerRadius(radius)
-	    .outerRadius(radius + (value[i] * 5) / 100)
+	    .innerRadius(0)
+	    .outerRadius(radius + (value[0][i] * 100) / 8)
 	    .startAngle(startAngle)
-	    .endAngle((2 * Math.PI) * (i + 1) / 4);
-	  startAngle = (2 * Math.PI) * (i + 1) / 4;
+	    .endAngle((2 * Math.PI) * (i - 1 + 1) / 4);
+	  startAngle = (2 * Math.PI) * (i - 1 + 1) / 4;
+	  
 	  roseGraph.append("path")
 	    .attr("class", "arc")
 	    .attr("d", arc)
@@ -65,12 +68,22 @@ function createRose(dataset){
 	    })
 	    .style("stroke", "#ffffff")
 	    .style("stroke-width", 2)
-	    .text(function(d) {
-	      return d;
-	    })
 	    .on("mouseover", function(d) {
-	      arc.style("fill", "#ffffff");
+	      
+	      arc.style("fill", "red");
+	    
 	    });
 
+	  var labels = roseGraph.selectAll("path")
+	  	.append("text")
+        .attr("dx", 100)
+        .attr("dy", 50)
+        .append("textPath")
+        /*.attr("xlink:href", function(d, i) {
+            return "#arc" + i;
+        })*/
+        .text(function(d) {
+            return categories(d);
+        })
 	}
 }
